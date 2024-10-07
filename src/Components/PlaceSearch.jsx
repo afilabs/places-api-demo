f28108b6ffa00bf45a598c0337cd5a81894b30c3
James La <james@afi.io>
2024-10-07 11:36:28 +0700

    upgrade @vis.gl/react-google-maps version

--- src/Components/PlaceSearch.jsx
+++ src/Components/PlaceSearch.jsx
@@ -61,7 +61,7 @@ const PlaceSearch = ({ onSelectPlace, onNearbyResultsReceived }) => {
         },
       );
     },
-    [placeService, onSelectPlace, searchNearbyPlaces],
+    [placeService],
   );
 
   return (
